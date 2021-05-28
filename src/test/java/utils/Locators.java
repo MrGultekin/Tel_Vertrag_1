bd4815712c7427da1694005fbbfcb1f5f5488ebc
MrGultekin <gultekin@gmail.com>
2021-05-28 10:21:43 +0200

    2021-May_28_Newsletter

--- src/test/java/utils/Locators.java
+++ src/test/java/utils/Locators.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 
 public class Locators {
     public static final String url = "https://telefonvertrag.de";
+    String email = "testuser@tutusmedia.com";
+
+
     public static final By contactFooterLink = By.xpath("//footer//a[text()='Kontakt & Support']");
 
     public static final By cookieAcceptButton = By.xpath("//button[@aria-label='Akzeptiere alle']");
@@ -18,8 +21,10 @@ public class Locators {
     public static final By errorNotifications = By.cssSelector(".srv-validation-message");
     public static final By alertSuccess = By.cssSelector("div[role='alert']");
 
-
-
+    // Newsletter input
+    public static final By newsletterEmailRegistrationInput = By.xpath("//input[@type='email']");
+    public static final By newsletterEmailRegistrationButton= By.cssSelector("button[class$='button']");
+    public static final By newsletterEmailUnRegistrationLink= By.xpath("//a[@title='Newsletter wieder abmelden']");
 
 
 
